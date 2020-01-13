1834070b780582f2e8ad75fa30a477132401a5a0
Willard Shikami <willardshikami@gmail.com>
2020-01-13 16:05:58 +0300

    setting up our model using Scoped Model

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:scoped_model/scoped_model.dart';
+
+void main() => runApp(new MyApp());
+
+class CounterModel extends Model {
+  int _count = 0;
+  int get count => _count;
+
+  void increment() {
+    _count++;
+    notifyListeners();
+  }
+
+  void decrement() {
+    _count++;
+    notifyListeners();
+  }
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Counter App',
+      home: ScopedModel<CounterModel>(
+        model: CounterModel(),
+        child: Home(),
+      ),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Counter App'),
+      ),
+
+    );
+  }
+}
