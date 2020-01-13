dd76a9610c7ca21c0db133e97c4b42b627b33f36
Willard Shikami <willardshikami@gmail.com>
2020-01-13 14:31:58 +0300

    project overhaul

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flight/main.dart';
+import 'package:flutterstate/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
